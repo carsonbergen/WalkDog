8d3673d9341677ec35030d883ee161642e13a16e
Carson Bergen <bergencarson@gmail.com>
2023-10-30 17:07:41 -0600

    Added Achievement component and examples

--- src/pages/testing.jsx
+++ src/pages/testing.jsx
@@ -1,31 +1,60 @@
 import { useEffect, useState } from "react";
 import TextInput from "../components/TextInput";
+import Achievement from "../components/Achievement";
+import { PawPrint } from "phosphor-react";
 
 export default function Testing() {
     const [text, setText] = useState('');
 
     useEffect(() => {
-        console.log("text was changed to ", text);
+        console.log("text was changed to: ", text);
+        // Now we can do whatever we want with this text
+        // It could be a email, password, or whatever really.
     }, [text]);
 
     return (
         <>
-            <TextInput>
-
-            </TextInput>
-            {/* 
-                How to set arguments for the text input. 
-            */}
+            <h1 className="text-3xl">
+                TextInput examples
+            </h1>
+            {/* Plain text input */}
+            <TextInput />
+            {/* How to set arguments for the text input */}
+            <TextInput
+                title="My TextInput with a title"
+            />
+            {/* TextInput with a function. This is the preferred way of doing this sort of thing. */}
             <TextInput 
-                title="My title"
+                title="My TextInput with a function (check the console logs)"
                 id="my text input"
                 value="test"
                 onChange={(e) => {
                     setText(e.target.value)
                 }}
                 placeholder="Hello, world!"
-            >
-            </TextInput>
+            />
+            <span>
+                This is our text: {text}
+            </span>
+
+            <h1 className="text-3xl">
+                Achievement examples
+            </h1>
+            <div className="flex flex-row space-x-2">
+                <Achievement 
+                    icon={<PawPrint size={32} weight="fill" />}
+                    description={
+                        `This is my test description for an achievement that was not given a title!`
+                    }
+                />
+                <Achievement 
+                    icon={<PawPrint size={32} weight="fill" />}
+                    title="Avid Walker"
+                    description={
+                        `You walked your dog over 5km!`
+                    }
+                />
+            </div>
         </>
     );
 }
