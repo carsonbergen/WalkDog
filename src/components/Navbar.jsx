5a584a32b7a48015d89b0851a8edb5f45e62e9ce
Carson Bergen <bergencarson@gmail.com>
2023-10-30 13:28:12 -0600

    Added paint-stroke css class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar(props) {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 256 256"
-                className="stroke-[1rem] fill-yellow stroke-black"
+                className="stroke-[2rem] fill-yellow stroke-black paint-stroke"
               >
                 <path d="M224,120v16a8,8,0,0,1-8,8H40a8,8,0,0,1-8-8V120a8,8,0,0,1,8-8H216A8,8,0,0,1,224,120Zm-8,56H40a8,8,0,0,0-8,8v16a8,8,0,0,0,8,8H216a8,8,0,0,0,8-8V184A8,8,0,0,0,216,176Zm0-128H40a8,8,0,0,0-8,8V72a8,8,0,0,0,8,8H216a8,8,0,0,0,8-8V56A8,8,0,0,0,216,48Z"></path>
               </svg>
@@ -31,7 +31,7 @@ export default function Navbar(props) {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 256 256"
-                className="stroke-[0.5rem] fill-yellow stroke-black"
+                className="stroke-[1rem] fill-yellow stroke-black paint-stroke"
               >
                 <path d="M240,108a28,28,0,1,1-28-28A28,28,0,0,1,240,108ZM72,108a28,28,0,1,0-28,28A28,28,0,0,0,72,108ZM92,88A28,28,0,1,0,64,60,28,28,0,0,0,92,88Zm72,0a28,28,0,1,0-28-28A28,28,0,0,0,164,88Zm23.12,60.86a35.3,35.3,0,0,1-16.87-21.14,44,44,0,0,0-84.5,0A35.25,35.25,0,0,1,69,148.82,40,40,0,0,0,88,224a39.48,39.48,0,0,0,15.52-3.13,64.09,64.09,0,0,1,48.87,0,40,40,0,0,0,34.73-72Z" />
               </svg>
@@ -45,7 +45,7 @@ export default function Navbar(props) {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 256 256"
-                className="stroke-[1rem] fill-yellow stroke-black"
+                className="stroke-[2rem] fill-yellow stroke-black paint-stroke"
               >
                 <path d="M230.93,220a8,8,0,0,1-6.93,4H32a8,8,0,0,1-6.92-12c15.23-26.33,38.7-45.21,66.09-54.16a72,72,0,1,1,73.66,0c27.39,8.95,50.86,27.83,66.09,54.16A8,8,0,0,1,230.93,220Z"></path>
               </svg>
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+
 export default function Topbar() {
     return (
         <>
-            <div className="absolute top-0 left-0 w-full h-20 bg-black">
+            <div className="absolute top-0 left-0 w-full py-2 px-8">
+                <div className="absolute bottom-0 left-0 z-0 w-full h-full py-2 px-8">
+                    <div className="h-full w-full border-2 border-secondary rounded-lg">
+                    </div>
+                </div>
+                <div className="relative flex flex-row justify-between w-full items-baseline px-1 py-1">
+                    <div className="flex-shrink-0 w-[25%] flex flex-row justify-center">
+                        <Link
+                            to="feed"
+                            className={`flex items-center justify-center rounded-lg bg-purple w-16 h-10 p-4 border-secondary border-2 stroke-2`}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="stroke-[2rem] fill-yellow stroke-black paint-stroke">
+                                <path d="M232.49,215.51,185,168a92.12,92.12,0,1,0-17,17l47.53,47.54a12,12,0,0,0,17-17ZM44,112a68,68,0,1,1,68,68A68.07,68.07,0,0,1,44,112Z"></path>
+                            </svg>
+                        </Link>
+                    </div>
+                    <div className="flex-grow flex flex-row justify-center">
+                        {/* Empty */}
+                    </div>
+                    <div className="flex-shrink-0 w-[25%] flex flex-row justify-center">
+                        <Link
+                            to="my-profile"
+                            className={`flex items-center justify-center rounded-lg bg-purple w-16 h-10 p-4 border-secondary border-2`}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="stroke-[2rem] fill-yellow stroke-black paint-stroke">
+                                <path d="M225.29,165.93C216.61,151,212,129.57,212,104a84,84,0,0,0-168,0c0,25.58-4.59,47-13.27,61.93A20.08,20.08,0,0,0,30.66,186,19.77,19.77,0,0,0,48,196H208a19.77,19.77,0,0,0,17.31-10A20.08,20.08,0,0,0,225.29,165.93ZM54.66,172C63.51,154,68,131.14,68,104a60,60,0,0,1,120,0c0,27.13,4.48,50,13.33,68ZM172,224a12,12,0,0,1-12,12H96a12,12,0,0,1,0-24h64A12,12,0,0,1,172,224Z"></path>
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </>
     );
