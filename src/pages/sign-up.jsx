f9c884935609ad9f9535f6ffbd695e5c6c74667e
allianad <alliana.dp@gmail.com>
2023-11-01 13:20:07 -0600

    add sign-up content

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,24 +1,74 @@
+import { AcceptButton, OptionalButton } from "../components/Button";
+import TextInput from "../components/TextInput";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
     return (
         <>
-            <div>
-                <h1>
+            <div 
+                className={`grid grid-cols-1 place-content-center px-10`}
+            >
+                <h1 className={`place-self-center pb-6 text-5xl font-black`}>
                     Walk. Dog.
                 </h1>
-                <body>
-                    Welcome back! We're glad to see you.
-                </body>
-                <form>
-                    <label>
-                        Username:
-                        <input type="text" name="username"/>
-                    </label>
-                    <label>
-                        Password:
-                        <input type="password" name="password" />
-                    </label>
-                    <input type="submit" value="Submit" />
+                <form className={`grid grid-cols-1 gap-4 place-content-center`}>
+                    <TextInput 
+                        title="What would you like to be called?"
+                        id="username"
+                        onChange={(e) => {
+                            setText(e.target.value)
+                        }}
+                        placeholder="John"
+                    />
+                    <TextInput 
+                        title="What do you call your canine companion?"
+                        id="dogName"
+                        onChange={(e) => {
+                            setText(e.target.value)
+                        }}
+                        placeholder="Juno"
+                    />
+                    <TextInput 
+                        title="What is your preferred email address?"
+                        id="email"
+                        onChange={(e) => {
+                            setText(e.target.value)
+                        }}
+                        placeholder="johnDoe@gmail.com"
+                    />
+                    <TextInput 
+                        title="What would you like your password to be?"
+                        id="password"
+                        onChange={(e) => {
+                            setText(e.target.value)
+                        }}
+                        type="password"
+                    />
+                    <div
+                        className={`w-full place-self-center mt-3 mb-14`}
+                    >
+                        <AcceptButton
+                        // TODO change to go to feed, and verification
+                            onClick={() => {
+                                console.log("Accept!")
+                            }}
+                        >
+                            Sign up!
+                        </AcceptButton>
+                    </div>
                 </form>
+                <div className={`place-self-center`}>
+                    <body
+                        className={`font-bold text-base  text-dark_grey`}
+                    >
+                        Already have an account?
+                    </body>
+                    <Link to="login">
+                        <OptionalButton>
+                            Log in!
+                        </OptionalButton>
+                    </Link>
+                </div>
             </div>
         </>
     );
