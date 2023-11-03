4162e7cbec769cc30d5467231d2fb272e0d271bf
rafael.galindo <rafael.galindo@ucalgary.ca>
2023-11-02 20:05:24 -0600

    Added My Stats section, My Achievements Section, and General Settings section

--- src/pages/my-profile.jsx
+++ src/pages/my-profile.jsx
@@ -3,7 +3,8 @@ import myImage from '../assets/images/profilePicture.jpg';
 import '../css/MyProfile.css';
 import TextInput from "../components/TextInput";
 import Achievement from '../components/Achievement';
-import { PawPrint } from 'phosphor-react';
+import { PawPrint, Aperture } from 'phosphor-react';
+import ToggleSwitch from '../components/ToggleSwitch';
 
 export default function MyProfile() {
     return (
@@ -22,16 +23,47 @@ export default function MyProfile() {
                     />
                 </div>
             </div>
-            <div className="section-header">My Stats</div>
-            <div className="section-header">My Achievements</div>
-            <Achievement
-                icon={<PawPrint size={32} weight="fill" />}
-                title="Avid Walker"
-                description={
-                    `You walked your dog over 5km!`
-                }
-            />
-            <div className="section-header">General Settings</div>
+            <div className="section-header font-bold">My Stats</div>
+            <div>
+                <div className='stats'>
+                    You've walked 
+                    [name of dog] 
+                    [times walked] 
+                    times in a row!
+                </div>
+                <div className='stats'>
+                    Total distance walked: 
+                    [distance walked]
+                </div>
+                <div className='stats'>
+                    Photos shared:
+                    [photos shared]
+                </div>
+            </div>
+            <div className="section-header font-bold">My Achievements</div>
+            <div className='flex flex-row space-x-2'> 
+                    <Achievement
+                        icon={<Aperture size={32} weight="fill" />}
+                        title="The Photographer"
+                        description={
+                            `You've taken 5 photos`
+                        }
+                    />
+                    <Achievement 
+                        icon={<PawPrint size={32} weight="fill" />}
+                        title="Avid Walker"
+                        description={
+                            `You walked your dog over 5km!`
+                        }
+                    />
+                </div>
+            <div className="section-header font-bold">General Settings</div>
+            <div className='flex flex-row space-x-2'>
+                Enable geolocation services<ToggleSwitch></ToggleSwitch> 
+            </div>
+            <div className='flex flex-row space-x-2'>
+                Enable geolocation services<ToggleSwitch></ToggleSwitch> 
+            </div>
         </>
     );
 }
