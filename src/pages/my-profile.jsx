85d5569359a3c09c66421d2d1ca79ab3b7f86f2b
rafael.galindo <rafael.galindo@ucalgary.ca>
2023-11-02 19:03:48 -0600

    Added a pictures folder, added a .css folder called MyProfile, added a picture in MyProfilePage and textboxes

--- src/pages/my-profile.jsx
+++ src/pages/my-profile.jsx
@@ -1,11 +1,25 @@
+import React from 'react';
+import myImage from '../assets/images/profilePicture.jpg';
+import '../css/MyProfile.css'; 
+
 export default function MyProfile() {
     return (
         <>
-            <div>
-                <h1>
-                    My Profile
-                </h1>
+            <div className="profile-container">
+                <div className="profile-image-container">
+                    <img src={myImage} alt="profilePicture" className="profile-image" />
+                </div>
+                <div className="profile-info">
+                    <div className="profile-header">Profile Display Settings</div>
+                    <div className="profile-label">Display name:</div>
+                    <input type="text" defaultValue="Alex" className="profile-input" />
+                    <div className="profile-label">Your dog's name:</div>
+                    <input type="text" defaultValue="Rex" className="profile-input" />
+                </div>
             </div>
+            <div className="section-header">My Stats</div>
+            <div className="section-header">My Achievements</div>
+            <div className="section-header">General Settings</div>
         </>
     );
 }
