376d422f71b8c2da995d56afa3a4f8d1c89f7998
Carson Bergen <bergencarson@gmail.com>
2023-11-02 19:32:09 -0600

    Replaced some stuff with components

--- src/pages/my-profile.jsx
+++ src/pages/my-profile.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import myImage from '../assets/images/profilePicture.jpg';
-import '../css/MyProfile.css'; 
+import '../css/MyProfile.css';
+import TextInput from "../components/TextInput";
+import Achievement from '../components/Achievement';
+import { PawPrint } from 'phosphor-react';
 
 export default function MyProfile() {
     return (
         <>
             <div className="profile-container">
-                <div className="profile-image-container">
-                    <img src={myImage} alt="profilePicture" className="profile-image" />
+                <div className="rounded-md border-2 border-secondary mr-4 max-w-[150px] max-h-[150px] overflow-clip">
+                    <img src={myImage} alt="profilePicture" className="" />
                 </div>
                 <div className="profile-info">
                     <div className="profile-header">Profile Display Settings</div>
-                    <div className="profile-label">Display name:</div>
-                    <input type="text" defaultValue="Alex" className="profile-input" />
-                    <div className="profile-label">Your dog's name:</div>
-                    <input type="text" defaultValue="Rex" className="profile-input" />
+                    <TextInput
+                        title="Display name:"
+                    />
+                    <TextInput
+                        title="Your dog's name:"
+                    />
                 </div>
             </div>
             <div className="section-header">My Stats</div>
             <div className="section-header">My Achievements</div>
+            <Achievement
+                icon={<PawPrint size={32} weight="fill" />}
+                title="Avid Walker"
+                description={
+                    `You walked your dog over 5km!`
+                }
+            />
             <div className="section-header">General Settings</div>
         </>
     );
