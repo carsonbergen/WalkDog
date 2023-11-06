a91776ee239d50349dcb70485f6b394a85a8bf3b
Carson Bergen <bergencarson@gmail.com>
2023-11-06 13:46:38 -0700

    Added image component

--- src/components/FeedItem.jsx
+++ src/components/FeedItem.jsx
@@ -1,4 +1,5 @@
 import { UserButton } from "./Button";
+import Image from "./Image";
 import { DateSpan, DistanceSpan, DogSpan, LocationSpan } from "./Misc/InlineComponents";
 
 export default function FeedItem(props) {
@@ -6,15 +7,15 @@ export default function FeedItem(props) {
         <>
             <div key={props.id} className="flex flex-row bg-pink border-secondary rounded-md border-2">
                 {props.imageSrc && (
-                    <img
+                    <Image
                         src={props.imageSrc}
-                        alt={`Image for post by ${props.author}`}
-                        className="post-image border-2 border-secondary rounded-md max-w-[100px] h-auto m-[-2px]"
+                        alt={props.imageAlt}
+                        className="border-r-2 rounded-r-md"
                     />
                 )}
                 <div className="font-nunito px-2 flex flex-wrap justify-start items-baseline space-y-1 py-1">
                     <UserButton to={props.profileLink}>{props.author}</UserButton>
-                    <span className="mx-1">walked</span> 
+                    <span className="mx-1">walked</span>
                     <DogSpan>{props.dog}</DogSpan>
                     <span className="mx-1">on</span>
                     <DateSpan>{props.date}</DateSpan>
