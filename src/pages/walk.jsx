a3f11065d13f72deb537cca560807332f084779e
Carson Bergen <bergencarson@gmail.com>
2023-11-27 13:39:33 -0700

    Added dog mood meter

--- src/pages/walk.jsx
+++ src/pages/walk.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Map from '../components/Map';
+import DogHappinessMeter from '../components/DogHappinessMeter';
 
 export default function Walk() {
     return (
         <>
-            {/* Dog model */}
-            {/* Map */}
             <div className='overflow-hidden h-screen w-full justify-center items-center pt-20 pb-24 px-4'>
+                <div className='absolute z-50 right-4 '>
+                    <DogHappinessMeter
+                        scale={0}
+                    />
+                </div>
                 <Map>
 
                 </Map>
