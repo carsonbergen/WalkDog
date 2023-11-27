acf67f17623b961b717c52452caa42e37fcfa698
Carson Bergen <bergencarson@gmail.com>
2023-11-27 13:23:21 -0700

    Map looks the same on all devices

---
+++ src/components/Map.jsx
@@ -0,0 +1,12 @@
+export default function Map() {
+    return (
+        <>
+            <div className="flex justify-center items-end h-full w-full">
+                <img
+                    className="w-full h-full rounded-lg border-2 border-secondary object-cover object-bottom"
+                    src="/placeholders/map.png"
+                />
+            </div>
+        </>
+    );
+}
---
+++ src/pages/walk.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Map from '../components/Map';
+
+export default function Walk() {
+    return (
+        <>
+            {/* Dog model */}
+            {/* Map */}
+            <div className='overflow-hidden h-screen w-full justify-center items-center pt-20 pb-24 px-4'>
+                <Map>
+
+                </Map>
+                {/* Start, pause, and etc. buttons */}
+            </div>
+        </>
+    );
+}
+
+
