78503603d5262fbf3fda43bd013e0bb739acff5d
Carson Bergen <bergencarson@gmail.com>
2023-12-04 00:58:49 -0700

    Added another achievement

--- src/lib/achievementChecker.js
+++ src/lib/achievementChecker.js
@@ -2,10 +2,26 @@ import achievements from "./../../database/achievements.json";
 import { getUserData, setUserData } from "./file";
 import Cookies from "js-cookie";
 
+// Add a function to check all requirements
+
 export function checkIfAchievementEarned(id) {
     const requirement = achievements[id].requirement;
     if (requirement.type === "photos_taken_in_a_walk") {
         const requirementMet = localStorage.getItem("photos_taken") >= requirement.value;
+        if (requirementMet) {
+            const email = Cookies.get("user");
+            addAchievementToUser(email, id);
+        }
+        return requirementMet;
+    } else if (requirement.type === "distance_walked") {
+        const requirementMet = localStorage.getItem("total_distance_walked") >= requirement.value;
+        if (requirementMet) {
+            const email = Cookies.get("user");
+            addAchievementToUser(email, id);
+        }
+        return requirementMet;
+    } else if (requirement.type === "less_than_distance_walked") {
+        const requirementMet = localStorage.getItem("total_distance_walked") < requirement.value;
         if (requirementMet) {
             const email = Cookies.get("user");
             addAchievementToUser(email, id);
