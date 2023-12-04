dbc34d632240bc9202662ceb80f44fadcf290700
Carson Bergen <bergencarson@gmail.com>
2023-12-04 01:32:55 -0700

    Fixed extra brace in my-profile

--- src/pages/my-profile.jsx
+++ src/pages/my-profile.jsx
@@ -107,7 +107,7 @@ export default function MyProfile() {
                         />
                         </div>
                     </>
-                )))}
+                ))}
             </div>
         </>
     );
